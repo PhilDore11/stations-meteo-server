e7e312872da9a7b6d7b7fa946f77f163e09a381f
Phil Dore <philippe.dore.11@gmail.com>
2019-01-13 15:16:58 -0500

    Add POST /clients to add Clients

---
+++ routes/clients.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : 'stations-meteo.c7abk9qcbiox.ca-central-1.rds.amazonaws.com',
+  port: 3306,
+  user : 'admin',
+  password : 'chanelle1',
+  database : 'stationsMeteoDB'
+});
+
+module.exports = {
+    connect: () => connection.connect(),
+    disconnect: () => connection.end(),
+    post: (req, res, next) => {
+      connection.query('INSERT INTO clients SET ?', req.body, (err, results, fields) => {
+        if (err) return next(err);
+
+        res.send(200);
+      });
+    },
+    get: (req, res, next) => {
+      connection.query('SELECT * from clients', (err, results, fields) => {
+        if (err) return next(err);
+
+        res.json(results);
+      });
+    }
+  }
--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const data = require('./data');
-
-const mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host : 'stations-meteo.c7abk9qcbiox.ca-central-1.rds.amazonaws.com',
-  port: 3306,
-  user : 'admin',
-  password : 'chanelle1',
-  database : 'stationsMeteoDB'
-});
+const clients = require('./clients');
 
 router.get('/data', data);
 
-router.get('/clients', (req, res, next) => {
-  connection.connect();
-  
-  connection.query('SELECT * from clients', (err, results, fields) => {
-    console.log('err', err);
-    console.log('results', results);
-    if (err) return next(err);
-    
-    connection.end();
-    
-    res.json(results);
-  });
-});
+clients.connect();
+router.get('/clients', clients.get);
+router.post('/clients', clients.post);
 
 module.exports = router;
