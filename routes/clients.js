5b2599a728bb07bf98ed0459933e76fddfb37f34
Phil Dore <philippe.dore.11@gmail.com>
2019-01-28 21:11:58 -0500

    Rework clients management

--- routes/clients.js
+++ routes/clients.js
@@ -1,47 +1,61 @@
 const db = require('./db');
 
+const _ = require('lodash');
+
+const clientGetQuery = "SELECT clients.id AS clientId, clients.name AS clientName, clients.email AS email, stations.id as stationId, stations.latitude AS latitude, stations.longitude AS longitude, stations.ipAddress AS ipAddress, stations.deviceType AS deviceType, stations.name AS stationName FROM clients JOIN stations ON stations.clientId = clients.id;"
+
 module.exports = {
   post: (req, res, next) => {
-    db.connection.query(
-      'INSERT INTO clients SET ?', 
-      req.body, 
-      (err) => {
-        if (err) return next(err);
+    db.connection.query('INSERT INTO clients SET ?', req.body, (err) => {
+      if (err) return next(err);
 
-        res.send(200);
-      }
-    );
+      res.send(200);
+    });
   },
   put: (req, res, next) => {
-    db.connection.query(
-      'UPDATE clients SET ? WHERE id=?', 
-      [req.body, req.body.id], 
-      (err) => {
-        if (err) return next(err);
+    db.connection.query('UPDATE clients SET ? WHERE id=?', [req.body, req.body.id], (err) => {
+      if (err) return next(err);
 
-        res.send(200);
-      }
-    );
+      res.send(200);
+    });
   },
   get: (req, res, next) => {
-    db.connection.query(
-      'SELECT * from clients', 
-      (err, results) => {
-        if (err) return next(err);
+    db.connection.query(clientGetQuery, (err, results) => {
+      if (err) return next(err);
 
-        res.json(results);
-      }
-    );
+      let clientResults = []
+      _.each(results, (result) => {
+        const clientResult = clientResults.find((item) => item.id === result.clientId);
+
+        const clientStation = {
+          id: result.stationId,
+          name: result.stationName,
+          latitude: result.latitude,
+          longitude: result.longitude,
+          ipAddress: result.ipAddress,
+          deviceType: result.deviceType,
+        };
+
+        if (clientResult) {
+          clientResult.stations.push(clientStation);
+        } else {
+          clientResults.push({
+            id: result.clientId,
+            name: result.clientName,
+            email: result.email,
+            stations: [clientStation]
+          })
+        }
+      })
+
+      res.json(clientResults);
+    });
   },
   delete: (req, res, next) => {
-    db.connection.query(
-      'DELETE from clients WHERE id=?', 
-      req.body.id, 
-      (err) => {
-        if (err) return next(err);
+    db.connection.query( 'DELETE from clients WHERE id=?', req.body.id, (err) => {
+      if (err) return next(err);
 
-        res.send(200);
-      }
-    );
+      res.send(200);
+    });
   },
 }
