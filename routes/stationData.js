0b3dcb1fe9fd46927421d934918b96118cb87181
Phil Dore <philippe.dore.11@gmail.com>
2019-02-09 09:11:12 -0500

    Rework dashboard graphs

--- routes/stationData.js
+++ routes/stationData.js
@@ -4,15 +4,24 @@ const db = require('./db');
 const constants = require('./constants');
 
 const getQuery =
-  'SELECT stationData.* FROM stationData JOIN stations ON stationData.stationId = stations.stationId JOIN clients ON stations.clientId = clients.id WHERE clients.id = ? AND (stationData.date BETWEEN ? AND ?)';
+  'SELECT MIN(stationData.date) as date, SUM(stationData.intensity) as intensity FROM stationData JOIN stations ON stationData.stationId = stations.stationId JOIN clients ON stations.clientId = clients.id WHERE clients.id = ? AND (stationData.date BETWEEN ? AND ?)';
 
 module.exports = {
   get: (req, res, next) => {
     const { clientId } = req.params;
-    const { start, end } = req.query;
+    const { start, end, view } = req.query;
+
+    let groupByClauses = [];
+    if (view === 'day') {
+      groupByClauses = ['YEAR(date)', 'MONTH(date)', 'DAY(date)', 'HOUR(date)', 'MINUTE(date)'];
+    } else if (view === 'week') {
+      groupByClauses = ['YEAR(date)', 'MONTH(date)', 'DAY(date)', 'HOUR(date)'];
+    } else {
+      groupByClauses = ['YEAR(date)', 'MONTH(date)', 'DAY(date)'];
+    }
 
     db.connection.query(
-      getQuery,
+      getQuery + ' GROUP BY ' + groupByClauses.join(', '),
       [
         parseInt(clientId),
         moment(start).format(constants.MYSQL_DATETIME_FORMAT),
