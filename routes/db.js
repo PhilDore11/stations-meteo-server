8d88a68d70e66ece2bbb28d73990b6744e209907
Philippe Dore <philippe.dore.11@gmail.com>
2020-05-20 10:24:09 -0400

    Update DB password

--- routes/db.js
+++ routes/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host : 'stations-meteo.c7abk9qcbiox.ca-central-1.rds.amazonaws.com',
   port: 3306,
   user : 'admin',
-  password : 'chanelle1',
+  password : 'adminPass',
   database : 'stationsMeteoDB'
 });
 
