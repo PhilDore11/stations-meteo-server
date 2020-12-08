d1d04b5dda2831697486040dc6374f6d67108b5b
Phil Doré <pdore@Apployeess-MacBook-Pro.local>
2020-12-08 16:33:21 -0500

    Fix TZ issue on server

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -1,14 +1,16 @@
-const moment = require("moment");
+const moment = require("moment-timezone");
 moment.locale("fr");
+
+const TIME_ZONE = "America/Toronto";
 
 const DATE_FORMAT = "YYYY-MM-DD";
 const DATETIME_FORMAT = `${DATE_FORMAT} HH:mm:ss`;
 
 module.exports = {
   convertToDateString: (momentValue) => {
-    return moment.utc(momentValue).local().format(DATE_FORMAT);
+    return moment.utc(momentValue).tz(TIME_ZONE).format(DATE_FORMAT);
   },
   convertToDateTimeString: (momentValue) => {
-    return moment.utc(momentValue).local().format(DATETIME_FORMAT);
+    return moment.utc(momentValue).tz(TIME_ZONE).format(DATETIME_FORMAT);
   },
 };
